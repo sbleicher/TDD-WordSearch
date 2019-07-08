cb51006c37398ade76b44f08d534c14d7a28ea03
Sean <sbleicher>
2019-07-07 23:56:58 -0400

    Master puzzle test added

--- tests/word-search.tests.cpp
+++ tests/word-search.tests.cpp
@@ -86,7 +86,8 @@ TEST(WordSearch, Horizantal_Forward_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(10,14), Position(11,14), Position(12,14), Position(13,14), Position(14,14) },
     { Position(6,0), Position(7,0), Position(8,0), Position(9,0) },
     { Position(7,10), Position(8,10), Position(9,10), Position(10,10) },
-    { Position(0,5), Position(1,5), Position(2,5), Position(3,5), Position(4,5), Position(5,5) } };
+    { Position(0,5), Position(1,5), Position(2,5), Position(3,5), Position(4,5), Position(5,5) } 
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
@@ -112,7 +113,8 @@ TEST(WordSearch, Horizantal_Backwards_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(2,0), Position(1,0), Position(0,0) },
     { Position(14,0), Position(13,0), Position(12,0) },
     { Position(2,14), Position(1,14), Position(0,14) },
-    { Position(14,14), Position(13,14), Position(12,14) }, };
+    { Position(14,14), Position(13,14), Position(12,14) }
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
@@ -138,7 +140,8 @@ TEST(WordSearch, Vertical_Forward_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(0,0), Position(0,1), Position(0,2) },
     { Position(14,0), Position(14,1), Position(14,2) },
     { Position(0,12), Position(0,13), Position(0,14) },
-    { Position(14,12), Position(14,13), Position(14,14) }, };
+    { Position(14,12), Position(14,13), Position(14,14) }
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
@@ -164,7 +167,8 @@ TEST(WordSearch, Diagonal_Down_Left_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(2,0), Position(1,1), Position(0,2) },
     { Position(14,0), Position(13,1), Position(12,2) },
     { Position(2,12), Position(1,13), Position(0,14) },
-    { Position(14,12), Position(13,13), Position(12,14) }, };
+    { Position(14,12), Position(13,13), Position(12,14) }
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
@@ -190,7 +194,8 @@ TEST(WordSearch, Diagonal_Down_Right_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(0,0), Position(1,1), Position(2,2) },
     { Position(12,0), Position(13,1), Position(14,2) },
     { Position(0,12), Position(1,13), Position(2,14) },
-    { Position(12,12), Position(13,13), Position(14,14) }, };
+    { Position(12,12), Position(13,13), Position(14,14) }
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
@@ -216,7 +221,38 @@ TEST(WordSearch, Diagonal_Up_Right_Test){
   const std::vector<std::vector<Position>> correctPositions = { { Position(0,2), Position(1,1), Position(2,0) },
     { Position(12,2), Position(13,1), Position(14,0) },
     { Position(0,14), Position(1,13), Position(2,12) },
-    { Position(12,14), Position(13,13), Position(14,12) }, };
+    { Position(12,14), Position(13,13), Position(14,12) }
+  };
+
+  WordSearch ws(filename);
+  ws.ReadFile();
+  ws.searchPuzzle();
+  std::vector<Word> words = ws.getWords();
+
+  ASSERT_EQ(correctPositions.size(), words.size());
+
+  for(int i = 0; i < words.size(); i++){
+    ASSERT_EQ(correctPositions[i].size(), words[i].word.size());
+  }
+
+  for(int i = 0; i < words.size(); i++){
+    for(int j = 0; j < words[i].word.size(); j++){
+      EXPECT_EQ(correctPositions[i][j].x, words[i].char_pos[j].x);
+      EXPECT_EQ(correctPositions[i][j].y, words[i].char_pos[j].y);
+    }
+  }
+}
+
+TEST(WordSearch, Master_Puzzle_Test){
+  const std::string filename = "test_data/master_puzzle.txt";
+  const std::vector<std::vector<Position>> correctPositions = { { Position(0,6), Position(0,7), Position(0,8), Position(0,9), Position(0,10) },
+    { Position(5,9), Position(5,8), Position(5,7), Position(5,6) },
+    { Position(4,7), Position(3,7), Position(2,7), Position(1,7) },
+    { Position(0,5), Position(1,5), Position(2,5), Position(3,5), Position(4,5), Position(5,5) },
+    { Position(2,1), Position(3,2), Position(4,3), Position(5,4), Position(6,5) },
+    { Position(3,3), Position(2,2), Position(1,1), Position(0,0) },
+    { Position(4,0), Position(3,1), Position(2,2), Position(1,3), Position(0,4) } 
+  };
 
   WordSearch ws(filename);
   ws.ReadFile();
